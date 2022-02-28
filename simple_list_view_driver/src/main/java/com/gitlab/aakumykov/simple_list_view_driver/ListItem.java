affb0efb8cc0c2a44d5532424aeeb5d0cfb4a5e2
Andrey Kumykov <aakumykov@yandex.ru>
2022-02-28 18:31:28 +0700

    Поле payload в ListItem.
    (cherry picked from commit 4916c7c3c6fc2904ed57b22bdbdf563dc314b17a)

--- simple_list_view_driver/src/main/java/com/gitlab/aakumykov/simple_list_view_driver/ListItem.java
+++ simple_list_view_driver/src/main/java/com/gitlab/aakumykov/simple_list_view_driver/ListItem.java
@@ -1,13 +1,23 @@
 package com.gitlab.aakumykov.simple_list_view_driver;
 
+import androidx.annotation.Nullable;
+
 public class ListItem implements iTitleItem {
 
     private final String mId;
     private final String mTitle;
+    @Nullable final Object mPayload;
 
     public ListItem(String id, String title) {
         mId = id;
         mTitle = title;
+        mPayload = null;
+    }
+
+    public ListItem(String id, String title, @Nullable Object payload) {
+        mId = id;
+        mTitle = title;
+        mPayload = payload;
     }
 
     @Override
@@ -17,5 +27,14 @@ public class ListItem implements iTitleItem {
 
     public String getId() {
         return mId;
+    }
+
+    public boolean hasPayload() {
+        return null != mPayload;
+    }
+
+    @Nullable
+    public Object getPayload() {
+        return mPayload;
     }
 }

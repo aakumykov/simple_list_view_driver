c5cc1373ddabf60ec8ec205e94b859ca911b69b2
Andrey Kumykov <aakumykov@yandex.ru>
2022-03-01 12:20:17 +0700

    Доделал демку для тестирования флагов прокрутки.

--- app/src/main/java/com/gitlab/aakumykov/simple_list_view_driver/MainActivity.java
+++ app/src/main/java/com/gitlab/aakumykov/simple_list_view_driver/MainActivity.java
@@ -1,66 +1,86 @@
 package com.gitlab.aakumykov.simple_list_view_driver;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.gitlab.aakumykov.simple_list_view_driver.databinding.ActivityMainBinding;
 
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private int mCounter = 1;
+    private ActivityMainBinding mViewBinding;
+    private SimpleListViewDriver mSimpleListViewDriver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        SimpleListViewDriver simpleListViewDriver = new SimpleListViewDriver(findViewById(R.id.listView));
+        mViewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mViewBinding.getRoot());
 
-        int b = mCounter++;
-        simpleListViewDriver.addItem(new ListItem(
-                UUID.randomUUID().toString(),
-                getString(R.string.element, b)
-        ));
 
-        simpleListViewDriver.setItemClickListener(titleItem -> {
+        mSimpleListViewDriver = new SimpleListViewDriver(findViewById(R.id.listView));
+
+        mSimpleListViewDriver.setItemClickListener(titleItem -> {
             String message = getString(R.string.click_on, titleItem.getTitle());
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         });
 
-        simpleListViewDriver.setItemLongClickListener(titleItem -> {
+        mSimpleListViewDriver.setItemLongClickListener(titleItem -> {
             String message = getString(R.string.long_click_on, titleItem.getTitle());
             Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
             return true;
         });
 
-        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        mSimpleListViewDriver.setDefaultScrollToNewItem(mViewBinding.scrollToNewItemsSwitch.isChecked());
 
-//        SwitchCompat switchCompat = ;
-//        simpleListViewDriver.setScrollToNewItem(switchCompat.isChecked());
 
-        floatingActionButton.setOnClickListener(v -> {
-            int c = mCounter++;
-            ListItem listItem = new ListItem(
-                    UUID.randomUUID().toString(),
-                    getString(R.string.element, c)
-            );
-            simpleListViewDriver.addItem(
-                    listItem,
-                    ((SwitchCompat)findViewById(R.id.scrollToNewItemsSwitch)).isChecked()
-            );
-            simpleListViewDriver.addItem(
-                    listItem,
-                    ((SwitchCompat)findViewById(R.id.scrollToNewItemsSwitch)).isChecked()
-            );
-            simpleListViewDriver.addItem(
-                    listItem,
-                    ((SwitchCompat)findViewById(R.id.scrollToNewItemsSwitch)).isChecked()
-            );
+        mViewBinding.scrollToNewItemsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mSimpleListViewDriver.setDefaultScrollToNewItem(isChecked);
+            }
         });
+
+        mViewBinding.addButton.setOnClickListener(this::onAddClicked);
+        mViewBinding.addWithScrollButton.setOnClickListener(this::onAddWithScrollClicked);
+
+
+        // Добавляю начальный элемент
+        /*int b = mCounter++;
+        mSimpleListViewDriver.addItem(new ListItem(
+                UUID.randomUUID().toString(),
+                getString(R.string.element, b)
+        ));*/
+        onAddWithScrollClicked(null);
+    }
+
+    private void onAddClicked(View view) {
+        ListItem listItem = createListItem();
+        for (int i=0; i<3; i++)
+            mSimpleListViewDriver.addItem(listItem);
+    }
+
+    private void onAddWithScrollClicked(View view) {
+        ListItem listItem = createListItem();
+        for (int i=0; i<3; i++)
+            mSimpleListViewDriver.addItem(
+                    listItem,
+                    true
+            );
+    }
+
+    private ListItem createListItem() {
+        int c = mCounter++;
+        return new ListItem(
+                UUID.randomUUID().toString(),
+                getString(R.string.element, c)
+        );
     }
 }
